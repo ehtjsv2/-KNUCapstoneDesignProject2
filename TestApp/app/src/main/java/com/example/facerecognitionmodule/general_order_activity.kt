46f3bb0a8c16a41daa22da13b7e8bdb782b96c70
ehtjsv2 <ehtjsv2@naver.com>
2023-05-16 22:45:30 +0900

    일반주문 layout 완성

--- TestApp/app/src/main/java/com/example/facerecognitionmodule/general_order_activity.kt
+++ TestApp/app/src/main/java/com/example/facerecognitionmodule/general_order_activity.kt
@@ -8,63 +8,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.LinearLayout
+import com.example.facerecognitionmodule.databinding.ActivityGeneralOrderBinding
 
 class general_order_activity : AppCompatActivity() {
-    private lateinit var selectedDrinks: MutableList<String>
-
+    lateinit var binding: ActivityGeneralOrderBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_general_order)
-
-        selectedDrinks = mutableListOf()
-
-        val drinkListLayout: LinearLayout = findViewById(R.id.drinkListLayout)
-        val completeButton: Button = findViewById(R.id.completeButton)
-        val backButton: Button = findViewById(R.id.backButton)
-
-        val buttons: List<Button> = listOf(
-            findViewById(R.id.americanoButton),
-            findViewById(R.id.cafeLatteButton),
-            findViewById(R.id.cappuccinoButton),
-            findViewById(R.id.coldBrewButton),
-            findViewById(R.id.caffeMochaButton)
-        )
+        binding =ActivityGeneralOrderBinding.inflate(layoutInflater)
 
 
-        // 초기 상태에서 버튼의 배경색을 변경
-        for (button in buttons) {
-            button.setBackgroundColor(Color.GRAY)
-        }
+        setContentView(binding.root)
 
-        for (button in buttons) {
-            button.setOnClickListener {
-                val drinkName = button.text.toString()
-
-                if (button.isSelected) {
-                    selectedDrinks.add(drinkName)
-                    button.isSelected = false
-                    button.setBackgroundColor(Color.GRAY)
-                } else {
-                    selectedDrinks.remove(drinkName)
-                    button.isSelected = true
-                    button.setBackgroundColor(resources.getColor(R.color.purple_500))
-                }
-            }
-        }
-
-        completeButton.setOnClickListener {
-            completeSelection()
-        }
-
-        backButton.setOnClickListener {
-            onBackPressed()
-        }
-    }
-
-    private fun completeSelection() {
-        val intent = Intent()
-        intent.putStringArrayListExtra("selectedDrinks", ArrayList(selectedDrinks))
-        setResult(Activity.RESULT_OK, intent)
-        finish()
     }
 }
