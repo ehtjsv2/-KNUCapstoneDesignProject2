2ed3e7aa394ea9e9244cefb575a26755c9ceb82c
ehtjsv2 <ehtjsv2@naver.com>
2023-05-17 19:52:05 +0900

    일반주문 layout 메뉴 크기 수정

--- TestApp/app/src/main/java/com/example/facerecognitionmodule/general_order_activity.kt
+++ TestApp/app/src/main/java/com/example/facerecognitionmodule/general_order_activity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.LinearLayout
@@ -15,7 +16,21 @@ class general_order_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =ActivityGeneralOrderBinding.inflate(layoutInflater)
-
+        binding.layoutMenu1.setOnClickListener {
+            Log.d("orderTest","menu1_click")
+        }
+        binding.layoutMenu2.setOnClickListener {
+            Log.d("orderTest","menu2_click")
+        }
+        binding.layoutMenu3.setOnClickListener {
+            Log.d("orderTest","menu3_click")
+        }
+        binding.layoutMenu4.setOnClickListener {
+            Log.d("orderTest","menu4_click")
+        }
+        binding.layoutMenu5.setOnClickListener {
+            Log.d("orderTest","menu5_click")
+        }
 
         setContentView(binding.root)
 
