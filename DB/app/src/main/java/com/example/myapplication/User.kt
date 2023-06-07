7b4ac992e5416c85f9d73fe6886ecca1b5da6e70
ehtjsv2 <ehtjsv2@naver.com>
2023-06-07 21:35:18 +0900

    delete unused files

--- DB/app/src/main/java/com/example/myapplication/User.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication
-
-import java.util.Vector
-
-data class User(val ID:String,val vector: DoubleArray, val americano_cnt:Int, val caffelatte_cnt:Int,
-                    val cappuccino_cnt:Int, val coldbrew_cnt:Int, val caffemocah_cnt:Int)
--- module/sdk/build/intermediates/cxx/Debug/yz4b4u4z/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Administrator\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\KNUCapstoneDesignProject2\\module\\sdk\\.cxx\\Debug\\yz4b4u4z\\x86_64" ^
-  opencv_jni_shared
